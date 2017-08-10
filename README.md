442dd1ae476c807e1f02be45ac64d119d02bb5da
Piotr Maliński <piotr.malinski@socialwifi.com>
2017-08-10 15:49:58 +0200

    Cut down to a library version.

--- README.md
+++ README.md
@@ -1,65 +1,12 @@
-# bmp2hex
+# bmp2hex as a library
 
-```
-Author:    		Robert Gallup (bg@robertgallup.com)
-Date:      		March 9, 2016
-License:   		MIT Opensource License (see license.txt) 
-Compatability: 	Python 2/3
-```
+This is a fork of Robert Gallup bmp2hex command line tool for
+converting BMP files into  byte arrays usable by some displays controlled by Arduino
+or similar microcontroller.
 
-Command line Python utility to output a table of hex values representing the size and data in a bmp graphics file. This would typically be used to create graphics for display by a microprocessor, say an Arduino, on an OLED or LCD.
+This fork just cuts down command line tools and returns a numeric list instead of
+printing C code with hexed values.
 
-The _input_ is a 1-bit .bmp file (color bmp files will not work)
-
-The _output_ is a valid C variable definition for  the Arduino. The bytes are defined as an array of const unsigned char. Since bitmaps can takea significant number of bytes, the PROGMEM keyword is used to place the data in program memory, rather than on the stack.
-
-Results from bmp2hex.py are directed to **standard output**. You can redirect them to a file, or use cut/paste to transfer the output to your code.
-
-### The command line is:
-
-``` bash
-$ python bmp2hex.py [-h] [-i] [-w WIDTH] [-b BYTESIZE] infile tablename
-```
-
-### Where:
-
-_-h, \-\-help_ = Help<br />
-_-i, \-\-invert_ = Invert image pixels<br />
-_WIDTH_ = Width of table in infile bytes (optional). [_Default = 16_]<br />
-_BYTESIZE_ = Number of bytes for size (optional). 0=auto, 1 or 2 (big endian) [_default = 0_]<br />
-_infile_ = Path to input bmp file<br />
-_tablename_ = Name to use for the output table<br />
-
-### Example:
-
-``` bash
-$ python bmp2hex.py -w 8 -b 2  soba.bmp SOBA
-```
-Process the file _soba.bmp_. Name the output table _SOBA_. Display the table with _8_ hex bytes on each row. Display the x/y sizes of the table using _2_ bytes, the first byte is the most significant (big endian).
-### Output:
-
-```
-const unsigned char PROGMEM SOBA [] = {
-0X00, 0X28, 0X00, 0X20,
-0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00,
-0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00,
-0X00, 0X00, 0X00, 0X80, 0X00, 0X00, 0X00, 0X01,
-0X90, 0X00, 0X00, 0X00, 0X03, 0X30, 0X00, 0X00,
-0X00, 0X06, 0X60, 0X00, 0X00, 0X00, 0X0C, 0XC0,
-0X00, 0X00, 0X00, 0X19, 0X80, 0X00, 0X1F, 0X00,
-0X33, 0X00, 0X00, 0X7B, 0XC0, 0X66, 0X00, 0X01,
-0XE0, 0XF0, 0XCC, 0X00, 0X03, 0X80, 0X39, 0X98,
-0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X07, 0XFF,
-0XFF, 0XFF, 0XE0, 0X07, 0XFF, 0XFF, 0XFF, 0XE0,
-0X07, 0XFF, 0XFF, 0XFF, 0XE0, 0X07, 0XFF, 0XFF,
-0XFF, 0XE0, 0X07, 0XFF, 0XFF, 0XFF, 0XE0, 0X03,
-0XFF, 0XFF, 0XFF, 0XC0, 0X03, 0XFF, 0XFF, 0XFF,
-0XC0, 0X01, 0XFF, 0XFF, 0XFF, 0X80, 0X01, 0XFF,
-0XFF, 0XFF, 0X80, 0X00, 0XFF, 0XFF, 0XFF, 0X00,
-0X00, 0X7F, 0XFF, 0XFE, 0X00, 0X00, 0X3F, 0XFF,
-0XFC, 0X00, 0X00, 0X1F, 0XFF, 0XF8, 0X00, 0X00,
-0X0F, 0XFF, 0XF0, 0X00, 0X00, 0X03, 0XFF, 0XC0,
-0X00, 0X00, 0X01, 0XFF, 0X80, 0X00, 0X00, 0X00,
-0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00
-};
-```
+Numbers from the list can be copy pasted to Arduino IDE or sent via for example UART
+to a working microcontroller for displaying on a connected display.
+Tested with ESP32 and Adafruit GFX library.
--- bmp2hex.py
+++ bmp2hex.py
@@ -1,135 +1,63 @@
 #!/usr/bin/env python
 
-##@file bmp2hex.py
+##@file b2h.py
 #  @ingroup util
-#	A script for converting a 1-bit bitmap to HEX for use in an Arduino sketch.
+#    A library for converting a 1-bit bitmap to HEX for use in an Arduino sketch.
 #
-#	The BMP format is well publicized. The byte order of the actual bitmap is a
-#	little unusual. The image is stored bottom to top, left to right. In addition,
-#	The pixel rows are rounded to DWORDS which are 4 bytes long. SO, to convert this
+#    The BMP format is well publicized. The byte order of the actual bitmap is a
+#    little unusual. The image is stored bottom to top, left to right. In addition,
+#    The pixel rows are rounded to DWORDS which are 4 bytes long. SO, to convert this
 #   to left to right, top to bottom, no byte padding. We have to do some calculations
-#	as we loop through the rows and bytes of the image. See below for more
+#    as we loop through the rows and bytes of the image. See below for more
+#    @author Robert Gallup 2016-02
 #
-#	Usage: 
-#	>>>bmp2hex.py [-i] [-w <bytes>] [-b <size-bytes>] <infile> <tablename>
-#	
-#	@param infile		The file to convert.
-#	@param tablename	The name of the table to create
-#   @param inverse		"-i", if invert image [optional] 
-#	@param tablewidth	"-w <bytes>, The number of characters for each row of the output table [optional]
-#	@param sizebytes	"-b <bytes>, Bytes = 0, 1, or 2. 0 = auto. 1 = 1-byte for sizes. 2 = 2-byte sizes (big endian) [optional]
-#	
-#	@author Robert Gallup 2016-02
+#    Author:    Robert Gallup (bg@robertgallup.com)
+#    License:   MIT Opensource License
 #
-#	Author:    Robert Gallup (bg@robertgallup.com)
-#	License:   MIT Opensource License
-#
-#	Copyright 2016 Robert Gallup 
+#    Copyright 2016 Robert Gallup 
 #
 
-import sys, array, os, textwrap, math, random, argparse
+import array
+import math
+import os
 
-def main ():
 
-	# Default parameters
-	infile = ""
-	tablename = ""
-	tablewidth = 16
-	sizebytes = 0
-	invert = False
+def bmp2hex(infile, invert=False):
+    invertbyte = 0x00 if invert else 0xFF
 
-	# Set up parser and handle arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument ("infile", help="The 1-bit BMP file to convert")
-	parser.add_argument ("tablename", help="The name of the output table")
-	parser.add_argument ("-i", "--invert", help="Inverts bitmap pixels", action="store_true")
-	parser.add_argument ("-w", "--width", help="Output table width in hex bytes [default: 16]", type=int)
-	parser.add_argument ("-b", "--bytes", help="Byte width of BMP sizes: 0=auto, 1, or 2 (big endian) [default: 0]", type=int)
-	args = parser.parse_args()
+    # Initilize output buffer
+    data_list = []
 
-	# Required arguments
-	infile = args.infile
-	tablename = args.tablename
+    # Open File
+    fin = open(os.path.expanduser(infile), "rb")
+    uint8_tstoread = os.path.getsize(os.path.expanduser(infile))
+    valuesfromfile = array.array('B')
+    try:
+        valuesfromfile.fromfile(fin, uint8_tstoread)
+    finally:
+        fin.close()
 
-	# Options
-	if args.invert:
-		invert = args.invert
-	if args.width:
-		tablewidth = args.width
-	if args.bytes:
-		sizebytes = args.bytes % 3
+    # Get bytes from file
+    values = valuesfromfile.tolist()
+    # Calculate and print pixel size
+    pixelWidth = getLONG(values, 18)
+    pixelHeight = getLONG(values, 22)
 
-	# Do the work
-	bmp2hex(infile, tablename, tablewidth, sizebytes, invert)
+    # Calculate width in words and padded word width (each row is padded to 4-bytes)
+    wordWidth = int(math.ceil(float(pixelWidth)/8.0))
+    paddedWidth = int(math.ceil(float(pixelWidth)/32.0) * 4)
 
-# Utility function. Return a long int from array (little endian)
+    # Get offset to BMP data (Byte 10 of the bmp data)
+    BMPOffset = getLONG(values, 10)
+
+    # Generate HEX bytes in output buffer
+    for i in range(pixelHeight):
+        for j in range (wordWidth):
+            ndx = BMPOffset + ((pixelHeight-1-i) * paddedWidth) + j
+            data_list.append(values[ndx] ^ invertbyte)
+
+    return data_list
+
+
 def getLONG(a, n):
-	return (a[n+3] * (2**24)) + (a[n+2] * (2**16)) + (a[n+1] * (2**8)) + (a[n])
-
-# Main conversion function
-def bmp2hex(infile, tablename, tablewidth, sizebytes, invert):
-
-	invertbyte = 0x00 if invert else 0xFF
-
-	# Convert tablewidth to characters from hex bytes
-	tablewidth = int(tablewidth) * 6
-
-	# Initilize output buffer
-	outstring =  ''
-
-	# Open File
-	fin = open(os.path.expanduser(infile), "rb")
-	uint8_tstoread = os.path.getsize(os.path.expanduser(infile))
-	valuesfromfile = array.array('B')
-	try:
-		valuesfromfile.fromfile(fin, uint8_tstoread)
-	finally:
-		fin.close()
-
-	# Get bytes from file
-	values=valuesfromfile.tolist()
-
-	# Calculate and print pixel size
-	pixelWidth  = getLONG(values, 18)
-	pixelHeight = getLONG(values, 22)
-
-	# Calculate width in words and padded word width (each row is padded to 4-bytes)
-	wordWidth   = int(math.ceil(float(pixelWidth)/8.0))
-	paddedWidth = int(math.ceil(float(pixelWidth)/32.0) * 4)
-
-	# For auto (sizebytes = 0), set sizebytes to 1 or 2, depending on size of the bitmap
-	if (sizebytes==0):
-		if (pixelWidth>255) or (pixelHeight>255):
-			sizebytes = 2
-		else:
-			sizebytes = 1
-
-	# Output the hex table declaration followed by the image x and y size
-	# sizebytes=1: image x/y are single byte sizes
-	# sizebytes=2: image x/y are double byte sizes (big endian)
-	print ('const unsigned char PROGMEM ' + tablename + ' [] = {')
-	if (not (sizebytes%2)):
-		print ("{0:#04X}".format((pixelWidth>>8) & 0xFF) + ", " + "{0:#04X}".format(pixelWidth & 0xFF) + ", " + \
-		      "{0:#04X}".format((pixelHeight>>8) & 0xFF) + ", " + "{0:#04X}".format(pixelHeight & 0xFF) + ",")
-	else:
-		print ("{0:#04X}".format(pixelWidth & 0xFF) + ", " + "{0:#04X}".format(pixelHeight & 0xFF) + ",")
-	
-	# Get offset to BMP data (Byte 10 of the bmp data)
-	BMPOffset = getLONG(values, 10)
-
-	# Generate HEX bytes in output buffer
-	try:
-		for i in range(pixelHeight):
-			for j in range (wordWidth):
-				ndx = BMPOffset + ((pixelHeight-1-i) * paddedWidth) + j
-				outstring += "{0:#04X}".format(values[ndx] ^ invertbyte) + ", "
-
-	# Wrap the output buffer. Print. Then, finish.
-	finally:
-		outstring = textwrap.fill(outstring[:-2], tablewidth)
-		print (outstring)
-		print ("};")
-
-
-# Only run if launched from commandline
-if __name__ == '__main__': main()
+    return (a[n+3] * (2**24)) + (a[n+2] * (2**16)) + (a[n+1] * (2**8)) + (a[n])
